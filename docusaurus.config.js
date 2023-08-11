7f26643072cabeff03806f241f801ef42d48dc9e
mehdi-ra <mehdirahimi.dev@gmail.com>
2023-08-11 20:44:25 +0330

    refactor(HomePageFeatures/index.tsx): Update features and removed one extra one

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'SelphJs',
-  tagline: 'Melt API on your hand',
+  tagline: 'Bring the power of api\'s in your hands.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
